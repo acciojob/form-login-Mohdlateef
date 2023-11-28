042361d7a147aeedb9adb9c84880d92090d276e4
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 22:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ function getFormvalue() {
 var value=document.getElementById("form1")
 	var first=value.elements["fname"].value;
 	var last=value.elements["lname"].value;
-	alert(first+""+last);
+	// var combine=first +"/n"+last;
+	// alert(combine);
+	alert(first+"\n"+last);
 }
